de65c220bf4f77604480416262eef140b6f225f6
alexandriamw <alexandriawagner84@gmail.com>
2019-10-06 20:36:29 -0500

    initial files

---
+++ server.js
@@ -0,0 +1,10 @@
+// Dependencies
+const express = require('express');
+const exphbs  = require('express-handlebars');
+const mongoose = require('mongoose');
+const cheerio = require('cheerio');
+const axios = require('axios');
+
+
+// Initialize Express
+const app = express();
